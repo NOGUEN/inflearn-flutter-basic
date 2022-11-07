ba17478201eace09c2e5ef4c46cd7c229534bf5f
NOGUEN <nhg1113@naver.com>
2022-11-07 19:07:20 +0900

    feat: Calendar added but not working now

--- one_line_diary/test/widget_test.dart
+++ one_line_diary/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:one_line_diary/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const OneLineDiary());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
